d5c946fe8801e60c5bf94d6d4fc82fc78d86852d
Luke Amos <luke.amos.24@ucl.ac.uk>
2025-01-24 14:36:22 +0000

    Adding command line argument for seed of rng.

--- Section_1_Monte_Carlo_Integrator/src/MonteCarlo.cpp
+++ Section_1_Monte_Carlo_Integrator/src/MonteCarlo.cpp
@@ -7,7 +7,7 @@ bool InsideUnitSphere(double x, double y, double z)
     return ((x*x + y*y + z*z) <= 1);
 }
 
-double IntegrateMonteCarlo3D(int n_points, double min, double max)
+double IntegrateMonteCarlo3D(int n_points, double min, double max, int seed)
 {
     int count = 0;
     // N.B. std::pow with integer arguments can be less efficient than
@@ -17,7 +17,7 @@ double IntegrateMonteCarlo3D(int n_points, double min, double max)
 
     //set up random number generator here
     std::uniform_real_distribution<double> uni_dist(-1.0, 1.0);
-    std::mt19937 rng_mt;
+    std::mt19937 rng_mt(seed);
 
     for(int i = 0; i < n_points; i++)
     {
@@ -32,11 +32,16 @@ double IntegrateMonteCarlo3D(int n_points, double min, double max)
 }
 
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc < 2) {
+        std::cout << "Give me a seed you donut." << std::endl;
+        return -1;
+    }
+    int seed = std::stoi(argv[1]);
     int N_points = 10000;
     
-    double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0);
+    double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0, seed);
 
     std::cout << "Volume estimate of sphere using " << N_points << " points = " << UnitSphereVol << std::endl;
 }
