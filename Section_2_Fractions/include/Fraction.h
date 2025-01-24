191870b69ef7bdeae45b51de5b5ad0bc59e0745f
Luke Amos <luke.amos.24@ucl.ac.uk>
2025-01-24 16:08:42 +0000

    Exercise 2 of Week 2.

--- Section_2_Fractions/include/Fraction.h
+++ Section_2_Fractions/include/Fraction.h
@@ -1,2 +1,14 @@
 #pragma once
 #include <string>
+
+class Fraction {
+    int num;
+    int denom;
+    public:
+
+    Fraction(int a, int b);
+    Fraction reciprocal();
+    Fraction multiply(int a);
+    double toDouble();
+    std::string toString();
+};
--- Section_2_Fractions/src/Fraction.cpp
+++ Section_2_Fractions/src/Fraction.cpp
@@ -1,1 +1,29 @@
 #include "Fraction.h"
+#include <numeric>
+
+Fraction::Fraction(int a, int b) {
+    int gcd = 1;
+    if (a!=0 && b!=0) {
+        gcd = std::gcd(a, b);
+    }
+    num = a/gcd;
+    denom = b/gcd;
+}
+
+Fraction Fraction::reciprocal() {
+    return Fraction(Fraction::num, Fraction::denom);
+}
+Fraction Fraction::multiply(int a) {
+    return Fraction(num * a, denom);
+}
+double Fraction::toDouble() {
+    return double(float(num) / float(denom));
+}
+
+std::string Fraction::toString() {
+    if (num==0) {
+        return "0";
+    }
+    return std::to_string(num) + "/" + std::to_string(denom);
+}
+
